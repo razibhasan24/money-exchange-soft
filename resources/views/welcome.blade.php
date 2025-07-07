2aadfc560ebaec4d24f086e14828298d59c1adb5
Razib-Rakib-Hasan <razibhasan2403@gmail.com>
2025-07-07 10:01:32 +0600

    some chnages

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>Lara Chini Chara</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
